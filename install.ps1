832fdb363cc38f13939e0dabc67297db9f300632
andreivlarox <39613681+andreivlarox@users.noreply.github.com>
2021-06-12 01:17:26 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@
 param(
     [Parameter(Position = 0)]
     [ValidateSet('Default', 'Flat', 'Mini')]
-    [string] $Layout = 'Default',
+    [string] $Layout = 'Mini',
     [Parameter()]
     [switch] $PreRelease
 )
